8bddbba6f89bb8f3934fa426b64fc3d7252f3bf5
BunMeat <varcmano57@gmail.com>
2024-10-13 03:07:08 +0700

    Revert "fix index.mjs and change main in package.json into index.mjs"
    This reverts commit 9199b52b072019fae572b93f54585ad57b10aee0.

--- filecloak/api/index.mjs
+++ filecloak/api/index.mjs
@@ -1,7 +1,4 @@
-// Load environment variables from .env file
-import dotenv from 'dotenv';
-dotenv.config(); // Correctly import and configure dotenv
-
+require('dotenv').config();
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
@@ -9,7 +6,6 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
 
-// Import your routers
 import indexRouter from './routes/index.js';
 import usersRouter from './routes/users.js';
 import testApi from './routes/testApi.js';
@@ -20,25 +16,8 @@ import encryptTextRouter from './routes/encryptText.js';
 import decryptRouter from './routes/decrypt.js';
 import dataListRouter from './routes/getDataList.js';
 
-// Import Firebase
-import { initializeApp } from 'firebase/app';
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
 const app = express();
 
-// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -50,7 +29,6 @@ app.use(
   })
 );
 
-// Define routes
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/testApi', testApi);
@@ -61,19 +39,18 @@ app.use('/api/encrypttext', encryptTextRouter);
 app.use('/api/decrypt', decryptRouter);
 app.use('/api/datalist', dataListRouter);
 
-// Catch 404 and forward to error handler
+// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// Error handler
+// error handler
 app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  
+
   res.status(err.status || 500);
-  res.json({ error: res.locals.message }); // Send JSON response for error
+  res.render('error');
 });
 
-// Export the app for use in server.js or index.mjs
 export default app;
