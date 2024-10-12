3bf073d6f61a5c4331c9a9488c03eed48f2ebf90
BunMeat <varcmano57@gmail.com>
2024-10-12 23:27:37 +0700

    delete views

--- filecloak/api/app.js
+++ filecloak/api/app.js
@@ -17,10 +17,6 @@ var decryptRouter = require('./routes/decrypt');
 var dataListRouter = require('./routes/getDataList')
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
