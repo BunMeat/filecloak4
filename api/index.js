deef72cc12c470b825aed61cd7a9d714d6909984
BunMeat <varcmano57@gmail.com>
2024-10-14 01:56:31 +0700

    change url in cors declaration

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: 'https://filecloak4.vercel.app', // Allow requests from your React app
+  origin: 'https://filecloak.vercel.app', // Allow requests from your React app
   methods: ["POST", "GET"],
   credentials: true
 }));
