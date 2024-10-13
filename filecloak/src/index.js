a2de5694743ce808d567025a42bf7a2ebd57782d
BunMeat <varcmano57@gmail.com>
2024-10-13 23:50:19 +0700

    move index.js to src

--- filecloak/src/index.js
+++ filecloak/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MainApp from './src/App.js';
+import MainApp from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
