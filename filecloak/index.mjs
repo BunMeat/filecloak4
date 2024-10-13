c668f206726b93058d11ab2a84d28479245cc1cd
BunMeat <varcmano57@gmail.com>
2024-10-13 22:15:42 +0700

    move index.mjs to root

--- filecloak/index.mjs
+++ filecloak/index.mjs
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import MainApp from './App.js';
+import MainApp from './src/App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
